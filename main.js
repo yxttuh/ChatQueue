daf26fcf7549eadee3418781327c8d9216d29ab0
Yxtt <81854177+yxttuh@users.noreply.github.com>
2025-12-18 00:06:08 +0000

    Add files via upload

--- main.js
+++ main.js
@@ -11,7 +11,7 @@ let isQueueOpen = true;
 
 const USER_AGENT = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36';
 
-// Helper to detect YouTube
+
 function isYouTube(url) {
     return url.includes('youtube.com') || url.includes('youtu.be');
 }
